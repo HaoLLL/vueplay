4fd673a2af422670e3f2306d3ccc6faf4d1a4ac1
郝雷 <m13522389185@163.com>
2021-02-26 00:19:36 +0800

    vue-props

---
+++ src/components/ButtonCounter.vue
@@ -0,0 +1,23 @@
+<template>
+    <button v-on:click="counter">You clicked me {{count}} times</button>
+</template>
+<script>
+// data must be a function
+// 
+export default {
+    data(){
+        return  {
+            count:0
+        }
+    },
+    methods:{
+        counter(){
+            this.count++;
+        }
+    }
+    
+}
+</script>
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,47 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo"
+      src="../assets/logo.png">
     <!-- <HelloWorld /> -->
     <!-- <Js /> -->
     <!-- <This /> -->
-    <String/>
+    <!-- <String/> -->
+    <blog-post 
+    v-for="post in posts"
+    v-bind:key="post.id"
+    v-bind:title="post.title">
+    </blog-post>
+    <!-- <button-counter></button-counter> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import Js from '@/components/Js.vue'
-import This from '@/components/This.vue'
-import String from '@/components/String.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import Js from "@/components/Js.vue";
+import This from "@/components/This.vue";
+import String from "@/components/String.vue";
+import ButtonCounter from "@/components/ButtonCounter.vue";
+import BlogPost from "@/components/BlogPost.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     HelloWorld,
     Js,
     This,
-    String
-  }
-}
+    String,
+    ButtonCounter,
+    BlogPost,
+  },
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "My journey with Vue" },
+        { id: 2, title: "Blogging with Vue" },
+        { id: 3, title: "Why Vue is so fun" },
+      ],
+    };
+  },
+};
 </script>
