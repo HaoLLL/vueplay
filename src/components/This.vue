3b46e90c9c9bb5840c1de8187d9c43aa6f9cf518
郝雷 <m13522389185@163.com>
2021-02-25 22:06:57 +0800

    about this

--- src/components/This.vue
+++ src/components/This.vue
@@ -3,6 +3,8 @@
     
 </template>
 <script>
+// 1. in method: this refers to the owner of the method(object)
+// 2. function.call(object) apply() transfor this to person2
 export default {
     data(){
         return {
@@ -11,11 +13,27 @@ export default {
     },
     methods:{
         test(){
+            var person = {
+                firstName:"Lei",
+                fullName:function(){
+                    return this.firstName;
+                }
+            };
+            var person1 = {
+                fullName:function(){
+                    return this.firstName + " "+ this.lastName;
+                }
+            }
+            var person2 = {
+               firstName:"Bill",
+               lastName:"Gates",
+            }
             
+            console.log(person1.fullName.call(person2));
         }
     },
     created () {
-        test()
+        this.test();
         
     }
     
