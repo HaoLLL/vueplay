ba605480da25e731dc39de93ba10f5a0296c7352
郝雷 <m13522389185@163.com>
2021-02-25 21:00:38 +0800

    js variable

--- src/components/Js.vue
+++ src/components/Js.vue
@@ -3,18 +3,29 @@
 <h1></h1>
     
 </template>
-<script>
-// string number boolean null undefined
+<script> 
 // var person = {}; new Object();
 // .property / ["property"]
 // for(var x in person){ console.log(person[x]); }
+// var x = 911+"Porsche" = "911"+"Porsche"
+// var x; undefined
+// typeof "Bill" typeof array = object typeof null = object
+// typeof undefined = undefined
+// null to be used to clear the object value
+// === to justify the type    ==  to justify the value
+// 原始数据 string number boolean undefined null symbol(es6)
+// 复杂数据 function object array
+// javascript = ECMAScript + DOM + BOM
+// type object array null = object
 export default {
     methods:{
         change(){
             var person = {firstName:'Bill', lastName:'Gates'};
             for(var p in person){
-                console.log(person[p]);
+                // console.log(person[p]);
             }
+            var x = 
+            console.log("sfa"+typeof x);
         }  
     },
     created(){
